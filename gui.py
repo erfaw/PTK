029fa4a2b274a177ab5b2b9a8aab31f4bfca1883
erfaw <erfawn.h@gmail.com>
2026-03-05 08:53:28 +0330

    refactor: attach some component to object (self)

--- gui.py
+++ gui.py
@@ -6,11 +6,11 @@ class Gui(Tk):
         self.base_font = ('Calibri', 18, 'normal')
         self.title("P-T-K")
         self.minsize(50,50)
-        self.resizable(False, False)
+        # self.resizable(False, False)
         self.config(bg= "gray", padx=10, pady=10)
 
-        titr_label_1 = Label(text= "Enter name of the program:", font= self.base_font, bg= "gray")
-        titr_label_1.grid()
+        self.titr_label_1 = Label(text= "Enter name of the program:", font= self.base_font, bg= "gray")
+        self.titr_label_1.grid()
 
         self.program_name_entry = Entry(
             width= 30,
@@ -19,8 +19,8 @@ class Gui(Tk):
         self.program_name_entry.focus()
         self.program_name_entry.grid()
 
-        titr_label_2 = Label(text= "Enter target time (e.g., 11:00 PM): ", font= self.base_font, bg= "gray")
-        titr_label_2.grid()
+        self.titr_label_2 = Label(text= "Enter target time (e.g., 11:00 PM): ", font= self.base_font, bg= "gray")
+        self.titr_label_2.grid()
 
         self.target_time_entry = Entry(
             width= 30,
@@ -28,10 +28,8 @@ class Gui(Tk):
         )
         self.target_time_entry.grid(pady=10)
 
-        accept_btn = Button(text= "Agree", width= 51, command= self.accept_btn_clicked)
-        accept_btn.grid()
-
-        self.mainloop()
+        self.accept_btn = Button(text= "Agree", width= 51, command= self.accept_btn_clicked)
+        self.accept_btn.grid()
     
     def accept_btn_clicked(self):
         self.program_name = self.program_name_entry.get().strip()
