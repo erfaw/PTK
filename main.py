d16ebf69b2933a7905da84701f91a0b75cedad5d
erfaw <erfawn.h@gmail.com>
2026-03-06 20:42:57 +0330

    feat: add popup messages
    -where ever was a print() statement , replace with ui.info() messagebox popups,
    -in order to do it we use if-else statement based on which value passed after run

--- main.py
+++ main.py
@@ -17,19 +17,20 @@ def main():
     target_time = ui.target_time_entry.get().strip()
     try:
         wait_time = timer.calculate_wait_time(target_time)
-        print(f"Program will close at {target_time}. Waiting...")
+        ui.info(f"Program will close at {target_time}. Waiting...")
     except ValueError:
-        print("Invalid time format. Please use the format 'HH:MM AM/PM'.")
+        ui.error("Invalid time format. Please use the format 'HH:MM AM/PM'.")
     else:
-        timer_after_id = None
         def count_down_timer(count):
             global timer_after_id
-            print(f"last:\t{count}")
             if count > 0 :
                 timer_after_id = ui.root.after(1000, count_down_timer, count-1 )
             else: 
                 ui.root.after_cancel(timer_after_id)
-                program_manager.kill(program_name= program_name)
+                if program_manager.kill(program_name):
+                    ui.info(f"<< {program_name} >> closed!")
+                else:
+                    ui.info(f"<{program_name}> not found.")
         count_down_timer(wait_time)
 
 ui.init_configs()
