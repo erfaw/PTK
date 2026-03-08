748239f31391127af823f4a7457988ad23c31317
erfaw <erfawn.h@gmail.com>
2026-03-08 15:35:55 +0330

    docs: commented try for notification icon

--- notification.py
+++ notification.py
@@ -1,7 +1,11 @@
 from plyer import notification
+from PIL import Image
+from pathlib import Path
 
 class Notification:
     def __init__(self):
+        # icon_fp = Path(__file__).resolve().parent / 'icon.ico'
+        # self.icon_image = Image.open(icon_fp)
         pass
 
     def send(self, message):
@@ -9,5 +13,6 @@ class Notification:
         notification.notify(
             title="PTK",
             message=message,
-            timeout=5  # Duration in seconds
+            timeout=5,  # Duration in seconds
+            # app_icon= self.icon_image
         )
