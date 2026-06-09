03eabbd374b8b7c85593a67e6906207aa94b583c
erfaw <erfawn.h@gmail.com>
2026-06-09 03:36:34 +0330

    refactor: open it with .bat with pythonw.exe to use in windows

--- launchers/launcher.bat
+++ launchers/launcher.bat
@@ -1,4 +1,4 @@
 @echo off
-python G:\myDocuments\Programming\Python\myApps\PTK6\main.py
-pause
-@REM TODO : make a ps1 instad of this to run as administrator 
+cd /d "G:\myDocuments\Programming\Python\myApps\PTK6"
+start "" pythonw.exe main.py
+exit
